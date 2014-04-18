16b261eed1e9bab98099e9382a2485c9aa82a1d9
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-04-17 20:03:13 -0700

    bit more testing, fixes

--- src/test/java/com/fasterxml/jackson/module/swagger/SimpleGenerationTest.java
+++ src/test/java/com/fasterxml/jackson/module/swagger/SimpleGenerationTest.java
@@ -1,12 +1,19 @@
 package com.fasterxml.jackson.module.swagger;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.*;
+
+import com.fasterxml.jackson.annotation.*;
+
+import com.fasterxml.jackson.databind.*;
+
 import com.fasterxml.jackson.module.swagger.model.Model;
+import com.fasterxml.jackson.module.swagger.model.ModelProperty;
+import com.wordnik.swagger.annotations.ApiModel;
 
 public class SimpleGenerationTest extends SwaggerTestBase
 {
 	@JsonPropertyOrder({ "a", "b" })
+	@ApiModel(description="DESC")
 	static class SimpleBean {
 		public int b;
 
@@ -20,6 +27,15 @@ public class SimpleGenerationTest extends SwaggerTestBase
 			.resolve(SimpleBean.class);
 		assertNotNull(model);
 
+		assertEquals("DESC", model.getDescription());
+		
+		List<ModelProperty> props = model.getProperties();
+		assertEquals(2, props.size());
+		
+		for (ModelProperty prop : props) {
+//			String name = prop.get
+		}
+		
 		/*
 		String asJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(model);
 		System.out.println("JSON: "+asJson);
